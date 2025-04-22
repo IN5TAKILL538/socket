16f64075da12c96c7dc6a3104e260d50e8d6c669
Cristian <galviscristian538@gmail.com>
2025-04-22 07:18:31 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
