471492839594076a1722c58747174615e11d72ae
Cristian <galviscristian538@gmail.com>
2025-04-22 07:25:13 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -72,9 +67,7 @@ io.on('connection', (socket) => {
 app.use(express.static('public'));
 
 // Opcional: Ruta para manejar todas las solicitudes y devolver index.html
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+
 
 
 const PORT = process.env.PORT || 5000;
